72af9511fd750f229cae4ef277d2227442427bcf
Fastidious <david@collantes.us>
2024-09-04 12:56:21 -0400

    Update main.go

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ func Version() string {
 func Usage() {
 	fmt.Printf("agevault %s", Version())
 	fmt.Println()
-	fmt.Println("lock/unlock directory with passphrase-protected identity file")
-	fmt.Println("usage: agevault [vault-name] lock|unlock|keygen")
+	fmt.Println("Lock/Unlock directory with passphrase-protected identity file")
+	fmt.Println("usage: agevault [directory-name] lock|unlock|keygen")
 	os.Exit(0)
 }
 
@@ -39,7 +39,7 @@ func getIdentityFilename(trimmedVaultName string) (string, error) {
 		return "", err
 	}
 	if identityFilename == "" {
-		return "", errors.New("missing identity file")
+		return "", errors.New("Missing identity file")
 	}
 	return identityFilename, nil
 }
@@ -68,7 +68,7 @@ func Lock(vaultName string, trimmedVaultName string) (string, error) {
 	encryptedFilename := fmt.Sprintf("%s.age", vaultName)
 	encryptedExists, encryptedIsDir := utils.Exists(encryptedFilename)
 	if encryptedExists && !encryptedIsDir {
-		return "", errors.New("already locked")
+		return "", errors.New("Already locked")
 	}
 	identityFilename, err := getIdentityFilename(trimmedVaultName)
 	if err != nil {
@@ -77,21 +77,21 @@ func Lock(vaultName string, trimmedVaultName string) (string, error) {
 	recipientString := strings.Split(identityFilename, ".")[1]
 	recipient, err := age.ParseX25519Recipient(recipientString)
 	if err != nil {
-		return "", fmt.Errorf("could not read recipient: %s", err.Error())
+		return "", fmt.Errorf("Could not read recipient: %s", err.Error())
 	}
 	vaultExists, vaultIsDir := utils.Exists(vaultName)
 	if !vaultExists || !vaultIsDir {
-		return "", fmt.Errorf("missing %s", vaultName)
+		return "", fmt.Errorf("Missing %s", vaultName)
 	}
 	var tarBuffer bytes.Buffer
 	if err = archive.TarDirectory(vaultName, &tarBuffer); err != nil {
-		return "", fmt.Errorf("could not tar: %s", err.Error())
+		return "", fmt.Errorf("Could not tar: %s", err.Error())
 	}
 	if err = crypt.EncryptToFile(encryptedFilename, tarBuffer.Bytes(), recipient); err != nil {
-		return "", fmt.Errorf("could not encrypt: %s", err.Error())
+		return "", fmt.Errorf("Could not encrypt: %s", err.Error())
 	}
 	if err = shredder.ShredDir(vaultName, 3); err != nil {
-		return "", fmt.Errorf("could not shred %s: %s", vaultName, err.Error())
+		return "", fmt.Errorf("Could not shred %s: %s", vaultName, err.Error())
 	}
 	return recipientString, nil
 }
@@ -103,19 +103,19 @@ func Unlock(vaultName string, trimmedVaultName string) error {
 	}
 	vaultExists, vaultIsDir := utils.Exists(vaultName)
 	if vaultExists && vaultIsDir {
-		return errors.New("already unlocked")
+		return errors.New("Already unlocked")
 	}
 	encryptedVaultFilename := fmt.Sprintf("%s.age", vaultName)
 	encryptedVault, err := os.Open(encryptedVaultFilename)
 	if err != nil {
-		return fmt.Errorf("missing encrypted %s: %s", vaultName, err.Error())
+		return fmt.Errorf("Missing locked %s: %s", vaultName, err.Error())
 	}
 	encryptedIdentity, err := os.Open(identityFilename)
 	if err != nil {
-		return fmt.Errorf("could not read identity file: %s", err.Error())
+		return fmt.Errorf("Could not read identity file: %s", err.Error())
 	}
 	pw, err := crypt.ReadSecret(
-		fmt.Sprintf("enter passphrase for identity file \"%s\"", identityFilename),
+		fmt.Sprintf("Enter passphrase for identity file \"%s\"", identityFilename),
 		false,
 	)
 	if err != nil {
@@ -124,16 +124,16 @@ func Unlock(vaultName string, trimmedVaultName string) error {
 	scryptIdentity, err := age.NewScryptIdentity(pw)
 	var identityBuffer bytes.Buffer
 	if err = crypt.DecryptToWriter(&identityBuffer, encryptedIdentity, scryptIdentity); err != nil {
-		return fmt.Errorf("bad passphrase: %s", err.Error())
+		return fmt.Errorf("Bad passphrase: %s", err.Error())
 	}
 	identity, err := age.ParseIdentities(&identityBuffer)
 	if err != nil || len(identity) != 1 {
-		return fmt.Errorf("could not parse decrypted identity: %s", err.Error())
+		return fmt.Errorf("Could not parse decrypted identity: %s", err.Error())
 	}
 	var tarBuffer bytes.Buffer
 	err = crypt.DecryptToWriter(&tarBuffer, encryptedVault, identity[0])
 	if err != nil {
-		return fmt.Errorf("could not decrypt %s: %s", vaultName, err.Error())
+		return fmt.Errorf("Could not decrypt %s: %s", vaultName, err.Error())
 	}
 	tarReader := bytes.NewReader(tarBuffer.Bytes())
 	if archive.IsZip(tarReader) {
@@ -141,15 +141,15 @@ func Unlock(vaultName string, trimmedVaultName string) error {
 		fmt.Println("found deprecated archiving format...")
 		zipReader := bytes.NewReader(tarBuffer.Bytes())
 		if err = archive.UnZip(*zipReader, "."); err != nil {
-			return fmt.Errorf("could not unzip zipped %s: %s", vaultName, err.Error())
+			return fmt.Errorf("Could not unzip zipped %s: %s", vaultName, err.Error())
 		}
 	} else {
 		if err = archive.UnTar(tarBuffer, "."); err != nil {
-			return fmt.Errorf("could not untar tarred %s: %s", vaultName, err.Error())
+			return fmt.Errorf("Could not untar tarred %s: %s", vaultName, err.Error())
 		}
 	}
 	if err = shredder.ShredFile(encryptedVaultFilename, 1); err != nil {
-		return fmt.Errorf("could not shred %s: %s", encryptedVaultFilename, err.Error())
+		return fmt.Errorf("Could not shred %s: %s", encryptedVaultFilename, err.Error())
 	}
 	return nil
 }
@@ -186,7 +186,7 @@ func main() {
 		if err != nil {
 			errMsg(err)
 		}
-		fmt.Printf("%s SECURED with %s\n", vaultName, recipientString)
+		fmt.Printf("%s LOCKED with %s\n", vaultName, recipientString)
 		return
 	}
 
@@ -195,7 +195,7 @@ func main() {
 		if err != nil {
 			errMsg(err)
 		}
-		fmt.Printf("%s DECRYPTED\n", vaultName)
+		fmt.Printf("%s UNLOCKED\n", vaultName)
 		return
 	}
 
